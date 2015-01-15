46f48fea3e8a8a61e028c4673e23a735437fd254
Eric Bailey <eric@ericb.me>
2015-01-15 17:15:40 -0600

    updated docs.sh

--- docs.sh
+++ docs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-lein marg -m && \
-mv docs/toc.html docs/index.html && \
+lein marg -m
+mv docs/toc.html docs/index.html
 sed -i 's/toc.html/index.html/' docs/*
+mv docs/* .
+git add *.html
+git checkout gh-pages
+git commit -m 'updated docs'
