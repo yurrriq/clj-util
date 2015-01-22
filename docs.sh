a25312bbd3a7b2e0903e6735b50c1291f466f258
Eric Bailey <eric@ericb.me>
2015-01-22 12:07:31 -0600

    updated build script

--- docs.sh
+++ docs.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-lein marg -m
-mv docs/toc.html docs/index.html
-sed -i 's/toc.html/index.html/' docs/*
-mv docs/* .
-git add *.html
-git checkout gh-pages
+lein marg -m -d _site
+mv _site/toc.html _site/index.html
+sed -i 's/toc.html/index.html/' _site/*
+
+cd _site
+git add .
 git commit -m 'updated docs'
+git push origin gh-pages
+
+cd ..
+git commit -a -m 'build site'
+git push origin master
